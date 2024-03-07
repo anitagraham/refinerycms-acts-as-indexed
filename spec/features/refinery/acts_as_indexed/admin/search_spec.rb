bc62fe0eab79d383fa4694f03fcedfc82fc129c7
Anita <anita@joli.com.au>
2024-03-07 13:14:29 +0800

    Test changes * using page_part.body rather than page_part.content as use of attribute_aliases has been made more stringent, and neither :body nor :content are attributes of page_part.
    * changed css selector for testing image search to be less strict but less ambiguous.

--- spec/features/refinery/acts_as_indexed/admin/search_spec.rb
+++ spec/features/refinery/acts_as_indexed/admin/search_spec.rb
@@ -1,11 +1,11 @@
 require "spec_helper"
 
 module Refinery
-  describe "search", :type => :feature do
+  describe "search", type: :feature do
 
     shared_examples "no result search" do
       it "returns no results" do
-        fill_in "search", :with => "yada yada"
+        fill_in "search", with: "yada yada"
         click_button "Search"
         expect(page).to have_content("Sorry, no results found")
       end
@@ -18,10 +18,10 @@ module Refinery
       end
 
       it "returns found image" do
-        fill_in "search", :with => "beach"
+        fill_in "search", with: "beach"
         click_button "Search"
 
-        within ".actions" do
+        within ".images_list" do
           expect(page).to have_selector("a[href*='#{image.image_name}']")
         end
       end
@@ -36,7 +36,7 @@ module Refinery
       end
 
       it "returns found resource" do
-        fill_in "search", :with => "cape"
+        fill_in "search", with: "cape"
         click_button "Search"
         expect(page).to have_content("Cape Town Tide Table")
       end
@@ -46,18 +46,18 @@ module Refinery
 
     describe "pages extension" do
       before do
-        FactoryBot.create(:page, :title => "Téléphone")
+        FactoryBot.create(:page, { title: "Téléphone" })
         visit refinery.admin_pages_path
       end
 
       it "returns found page without accents in the query" do
-        fill_in "search", :with => "Telephone"
+        fill_in "search", with: "Telephone"
         click_button "Search"
         expect(page).to have_content("Téléphone")
       end
 
       it "returns found page with accents in the query" do
-        fill_in "search", :with => "Téléphone"
+        fill_in "search", with: "Téléphone"
         click_button "Search"
         expect(page).to have_content("Téléphone")
       end
--- spec/models/refinery/page_spec.rb
+++ spec/models/refinery/page_spec.rb
@@ -1,13 +1,13 @@
 require 'spec_helper'
 
 module Refinery
-  describe Page, :type => :model do
+  describe Page, type: :model do
     let(:page_title) { 'RSpec is great for testing too' }
-    let(:page) { subject.class.new(:title => page_title, :deletable => true)}
+    let(:page) { subject.class.new( title: page_title, deletable: true) }
 
     before do
-      page.parts.new(:title => 'body', :content => "I'm the first page part for this page.", :position => 0)
-      page.parts.new(:title => 'side body', :content => 'Closely followed by the second page part.', :position => 1)
+      page.parts.new( title: 'body', body: "I'm the first page part for this page.", position: 0 )
+      page.parts.new( title: 'side body', body: 'Closely followed by the second page part.', position: 1)
     end
 
     it 'returns all page part content' do
